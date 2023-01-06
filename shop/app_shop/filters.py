05c4703ac5ad33d96f403f92aefb5bdcfbaa1dd2
Konstantin Ivanov <1cbit3@gmail.com>
2023-01-06 16:47:33 +0300

    fix a migrations bug

--- shop/app_shop/filters.py
+++ shop/app_shop/filters.py
@@ -1,5 +1,8 @@
+from contextlib import suppress
+
 import django_filters
 from django import forms
+from django.db import OperationalError
 from django_property_filter import PropertyBooleanFilter, PropertyFilterSet, PropertyOrderingFilter
 
 from .models import Product
@@ -9,7 +12,10 @@ from app_settings.models import SiteSettings
 
 
 class ProductFilter(PropertyFilterSet):
-    settings = SiteSettings.load()
+    root_category = 1
+    with suppress(OperationalError):
+        settings = SiteSettings.load()
+        root_category = settings.root_category
     name = django_filters.CharFilter(field_name='name', lookup_expr='icontains', widget=forms.TextInput(
         attrs={'class': 'form-input form-input_full',
                'placeholder': 'Название'}))
@@ -23,9 +29,9 @@ class ProductFilter(PropertyFilterSet):
                                       widget=forms.TextInput(attrs={'class': 'range-line',
                                                                     'data-type': 'double',
                                                                     'data-min': get_data_min(
-                                                                        root_category=settings.root_category),
+                                                                        root_category=root_category),
                                                                     'data-max': get_data_max(
-                                                                        root_category=settings.root_category)}))
+                                                                        root_category=root_category)}))
 
     order_by_field = 'ordering'
     ordering = PropertyOrderingFilter(
--- shop/app_shop/utils_shop.py
+++ shop/app_shop/utils_shop.py
@@ -1,3 +1,5 @@
+from contextlib import suppress
+
 from django.db import OperationalError
 from django.db.models import Min, Max, QuerySet
 from .models import Product, Category
@@ -10,13 +12,11 @@ def get_data_min(root_category: Category, queryset: QuerySet = None) -> int:
     if queryset:
         min_price = queryset.values('price').aggregate(Min('price'))['price__min']
     else:
-        try:
+        with suppress(AttributeError):
             sub_categories = root_category.get_descendants(include_self=True)
             min_price = \
                 Product.objects.values('price').filter(category__in=sub_categories).filter(available=True).aggregate(
                     Min('price'))['price__min']
-        except (AttributeError, OperationalError) as exc:
-            pass
     return int(min_price)
 
 
@@ -25,11 +25,9 @@ def get_data_max(root_category, queryset: QuerySet = None) -> int:
     if queryset:
         max_price = queryset.values('price').aggregate(Max('price'))['price__max']
     else:
-        try:
+        with suppress(AttributeError):
             sub_categories = root_category.get_descendants(include_self=True)
             max_price = \
                 Product.objects.values('price').filter(category__in=sub_categories).filter(available=True).aggregate(
                     Max('price'))['price__max']
-        except (AttributeError, OperationalError) as exc:
-            pass
     return max_price
